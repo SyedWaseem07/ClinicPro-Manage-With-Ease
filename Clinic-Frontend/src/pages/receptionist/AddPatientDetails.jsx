2868205b8871c3b25c9eac6d88d72db1d9814097
SyedWaseem07 <syedarw4794@gmail.com>
2024-07-04 12:42:34 +0530

    Add update patient and add patient details functionality

--- Clinic-Frontend/src/pages/receptionist/AddPatientDetails.jsx
+++ Clinic-Frontend/src/pages/receptionist/AddPatientDetails.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Form from '../../components/Form';
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const AddPatientDetails = () => {
   const [formData, setFormData] = useState({
@@ -26,12 +29,100 @@ const AddPatientDetails = () => {
     "amount": '',
     "date": ''
   })
-  const [flag, setFlag] = useState(false);
-  useEffect(() => {
-    console.log(formData, medicine, report, payment);
-  }, [flag])
+  const [step1Submit, setStep1Submit] = useState(false);
+  const [step2Submit, setStep2Submit] = useState(false);
+  const queryClient = useQueryClient();
+  const { mutate: step1Call, isPending: step1Loading } = useMutation({
+    mutationFn: async (formData) => {
+      try {
+        console.log("api call", { ...formData })
+        const res = await axios.post('/api/v1/users/receptionist/addPatientDetails', { ...formData });
+        return res.data.data;
+      } catch (error) {
+        console.log(error)
+        const index = error.response.data.indexOf("<pre>")
+        const Lastindex = error.response.data.indexOf("<br>")
+        const errMsg = error.response.data.substring(index + 5, Lastindex);
+        throw new Error(errMsg);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Step-1 completed successfully");
+      setStep1Submit(true);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addMedicineCall, isLoading: medicineLoading } = useMutation({
+    mutationFn: async (medicine) => {
+      try {
+        const res = await axios.post('/api/v1/users/receptionist/addMedicine', { ...medicine });
+        return res.data.data;
+      } catch (error) {
+        const index = error.response.data.indexOf("<pre>")
+        const Lastindex = error.response.data.indexOf("<br>")
+        const errMsg = error.response.data.substring(index + 5, Lastindex);
+        throw new Error(errMsg);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Medcine added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] });
+      setStep2Submit(true);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addReportCall, isPending: reportLoading } = useMutation({
+    mutationFn: async (report) => {
+      try {
+        console.log("from api", report)
+        const res = await axios.post('/api/v1/users/receptionist/addReport', report);
+        return res.data.data;
+      } catch (error) {
+        const index = error.response.data.indexOf("<pre>")
+        const Lastindex = error.response.data.indexOf("<br>")
+        const errMsg = error.response.data.substring(index + 5, Lastindex);
+        throw new Error(errMsg);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Report added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] })
+      setStep2Submit(true);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addPaymentCall, isPending: paymentLoading } = useMutation({
+    mutationFn: async (payment) => {
+      try {
+        console.log(payment);
+        const res = await axios.post('/api/v1/users/receptionist/addPaymentDetails', payment);
+        return res.data.data;
+      } catch (error) {
+        const index = error.response.data.indexOf("<pre>")
+        const Lastindex = error.response.data.indexOf("<br>")
+        const errMsg = error.response.data.substring(index + 5, Lastindex);
+        throw new Error(errMsg);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Payment added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] })
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
   return (
-    <Form formData={formData} setFormData={setFormData} medicine={medicine} setMedicine={setMedicine} report={report} setReport={setReport} payment={payment} setPayment={setPayment} setFlag={setFlag} />
+    <Form formData={formData} setFormData={setFormData} medicine={medicine} setMedicine={setMedicine} report={report} setReport={setReport} payment={payment} setPayment={setPayment} step1Call={step1Call} step1Loading={step1Loading} addMedicineCall={addMedicineCall} medicineLoading={medicineLoading} addReportCall={addReportCall} reportLoading={reportLoading} addPaymentCall={addPaymentCall} paymentLoading={paymentLoading} step1Submit={step1Submit} setStep1Submit={setStep1Submit} step2Submit={step2Submit} setStep2Submit={setStep2Submit} />
   )
 }
 
--- Clinic-Frontend/src/pages/receptionist/UpdatePatientDetails.jsx
+++ Clinic-Frontend/src/pages/receptionist/UpdatePatientDetails.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Form from '../../components/Form'
-
-const UpdatePatientDetails = ({ patientData }) => {
-  const [formData, setFormData] = useState({})
+import { useUpdatePatientsContext } from "../../context/UpdatePatient.context"
+import { useParams, NavLink } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+const UpdatePatientDetails = () => {
+  const [firstTime, setFirstTime] = useState(true);
+  const [formData, setFormData] = useState({
+    "patient_name": "",
+    "mobile_no": "",
+    "age": 0,
+    "weight": 0,
+    "symptoms": "",
+    "last_visited": "",
+    "gender": ""
+  })
   const [medicine, setMedicine] = useState({
     "patient_name": "",
     "medicine_name": "",
@@ -18,8 +31,87 @@ const UpdatePatientDetails = ({ patientData }) => {
     "amount": '',
     "date": ''
   })
+  const queryClient = useQueryClient();
+
+
+  const { mutate: step1Call, isLoading: step1Loading } = useMutation({
+    mutationFn: async (formData) => {
+      try {
+        console.log("api call", { ...formData })
+        const res = await axios.post('/api/v1/users/receptionist/updatePatientDetails', { ...formData });
+        return res.data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Step-1 completed successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addMedicineCall, isLoading: medicineLoading } = useMutation({
+    mutationFn: async (medicine) => {
+      try {
+        const res = await axios.post('/api/v1/users/receptionist/addMedicine', { ...medicine });
+        return res.data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Medcine added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] })
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addReportCall, isLoading: reportLoading } = useMutation({
+    mutationFn: async (report) => {
+      try {
+        console.log("from api", report)
+        const res = await axios.post('/api/v1/users/receptionist/addReport', report);
+        return res.data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Report added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] })
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
+
+  const { mutate: addPaymentCall, isLoading: paymentLoading } = useMutation({
+    mutationFn: async (payment) => {
+      try {
+        console.log(payment);
+        const res = await axios.post('/api/v1/users/receptionist/addPaymentDetails', payment);
+        return res.data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success("Payment added successfully");
+      queryClient.invalidateQueries({ queryKey: ['allPatients'] })
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  })
   return (
-    <Form formData={formData} setFormData={setFormData} fromUpdatePatient={true} medicine={medicine} setMedicine={setMedicine} report={report} setReport={setReport} payment={payment} setPayment={setPayment} />
+    <>
+      <Form formData={formData} setFormData={setFormData} fromUpdatePatient={true} medicine={medicine} setMedicine={setMedicine} report={report} setReport={setReport} payment={payment} setPayment={setPayment} step1Call={step1Call} step1Loading={step1Loading} addMedicineCall={addMedicineCall} medicineLoading={medicineLoading} addReportCall={addReportCall} reportLoading={reportLoading} firstTime={firstTime} setFirstTime={setFirstTime} addPaymentCall={addPaymentCall} paymentLoading={paymentLoading} />
+
+    </>
   )
 }
 
