dd99c6da5757967ae0baf2fe453452c2e17165a9
SyedWaseem07 <syedarw4794@gmail.com>
2024-07-09 22:12:11 +0530

    Modified app.js

--- Clinic-Backend/src/app.js
+++ Clinic-Backend/src/app.js
@@ -5,13 +5,7 @@ import userRouter from "./routes/user.routes.js"
 import receiptionistRouter from "./routes/receiptionist.routes.js"
 import doctorRouter from "./routes/doctor.routes.js"
 
-import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
-import postmanToOpenApi from "postman-to-openapi";
-
 const app = express()
-
-import { openapi } from "../openapi.js"
 
 app.use(cors());
 
@@ -25,43 +19,5 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/users/receptionist", receiptionistRouter);
 app.use("/api/v1/users/doctor", doctorRouter);
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapi));
-
-app.get('/swagger-json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerJson);
-});
-
-app.get('/generate-yml', async (req, res) => {
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // Postman Collection Path
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    const postmanCollection = 'collection.json'
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // Output OpenAPI Path
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    const outputFile = 'collection.yml'
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // Async/await
-    ////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    try {
-        const result = await postmanToOpenApi(postmanCollection, outputFile, {
-            defaultTag: 'General'
-        })
-        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        // Without save the result in a file
-        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        const result2 = await postmanToOpenApi(postmanCollection, null, {
-            defaultTag: 'General'
-        })
-        console.log(`OpenAPI specs: ${result}`)
-    } catch (err) {
-        console.log(err)
-    }
-});
-app.get('/', (req, res) => {
-    res.send("Welcome to backend of clinic management system developed by --Syed Waseem(Code Surgery Squad)");
-})
 export { app }
 
