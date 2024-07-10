deada74bf53f524c0dfa3e2fb70505a5b034e91f
SyedWaseem07 <syedarw4794@gmail.com>
2024-07-10 14:50:30 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,9 @@
-import path, { dirname } from "path";
+
 import dotenv from "dotenv"
 import { app } from "./app.js"
 import connectToDb from "./db/index.js"
-import express from "express"
 
 dotenv.config()
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
